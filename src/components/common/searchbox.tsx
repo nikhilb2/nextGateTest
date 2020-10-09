949f0eccc95f1fb29119b2bf58c7367d182ca67b
Nikhil Bhatia <nikhilbhatia2004@gmail.com>
2020-10-09 14:00:32 +0200

    fix: searchbox color fix

---
+++ src/components/common/searchbox.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import SearchIcon from '@material-ui/icons/Search'
+import InputBase from '@material-ui/core/InputBase'
+import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles'
+import { CSSProperties } from '@material-ui/core/styles/withStyles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade('rgba(255, 255, 255, 0.62)', 0.15),
+      '&:hover': {
+        backgroundColor: fade('rgba(255, 255, 255, 0.62)', 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  })
+)
+
+interface Props {
+  className?: string
+}
+
+const Searchbar = (props: Props) => {
+  const classes = useStyles()
+  const { className } = props
+ // const { filterApi } = props
+  return (
+    <div className={`${classes.search} ${className}`} >
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+      <InputBase
+        placeholder="Search…"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ 'aria-label': 'search' }}
+        onChange={(e) => {}}
+      />
+    </div>
+  )
+}
+
+export default Searchbar
--- src/components/headers/header.tsx
+++ src/components/headers/header.tsx
@@ -3,20 +3,35 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles'
-import theme from 'theme'
+import Searchbox from 'components/common/searchbox'
+import theme from 'theme';
 
 const useStyles = makeStyles({
     container: {
         backgroundColor: 'transparent',
         boxShadow: 'none'
+    },
+    title: {
+        letterSpacing: -.24
+    },
+    toolBar: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
+
+    },
+    searchBox: {
+        marginRight: theme.spacing(4)
     }
 })
 const Header = () => {
     const classes = useStyles()
     return(
         <AppBar className={classes.container} >
-        <Toolbar>
-          <Typography variant="h6">Scroll to see button</Typography>
+        <Toolbar className={classes.toolBar}>
+          <Typography variant="h5" className={classes.title}>NextGateTech Firebase Test</Typography>
+          <Searchbox className={classes.searchBox} />
         </Toolbar>
       </AppBar>
     )
